1ee180bacc301b28e63393dbe0c2ed16daf5fe19
kosuke9809 <uht0912k@outlook.com>
2022-04-20 11:37:45 +0900

    windows

--- api/word2vec/word2vec.py
+++ api/word2vec/word2vec.py
@@ -1,10 +1,13 @@
+import os 
 import gensim
+from dotenv import load_dotenv
 
+load_dotenv()
 class Word2VecAPI:
   def __init__(self):
     self.word2vec = None
   def load(self):
-    self.word2vec = gensim.models.word2vec.KeyedVectors.load_word2vec_format('/Users/uehatakosuke/dev_es/backend/api/word2vec/model.vec', binary=False)
+    self.word2vec = gensim.models.word2vec.KeyedVectors.load_word2vec_format(os.environ['WORD2VEC_PATH'], binary=False)
     
   def similarity_words(self,keyword:str,word:str) -> str:
     word2vec = self.word2vec
